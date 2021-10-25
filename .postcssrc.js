38c3523c529c58e87ebeb8843856122be05a0b67
clearself <fuleiyang@qq.com>
2021-10-25 17:08:26 +0800

    提交

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,17 +1,29 @@
-module.exports = ({ file }) => {
-    let rootValue;
-    // 若为vant组件 则根fz为37.5 其他则设置为75
-    if (file && file.dirname && file.dirname.indexOf("vant") > -1) {
-        rootValue = 37.5;
-    } else {
-        rootValue = 75;
-    }
-    return {
-      plugins: {
-        'postcss-pxtorem':{
-            rootValue: rootValue,
-             propList: ["*"] 
-        }
-      }
-    };
+// module.exports = ({ file }) => {
+//     let rootValue;
+//     // 若为vant组件 则根fz为37.5 其他则设置为75
+//     if (file && file.dirname && file.dirname.indexOf("vant") > -1) {
+//         rootValue = 37.5;
+//     } else {
+//         rootValue = 75;
+//     }
+//     return {
+//       plugins: {
+//         'postcss-pxtorem':{
+//             rootValue: rootValue,
+//              propList: ["*"] 
+//         }
+//       }
+//     };
+//   };
+
+module.exports = {
+    plugins: {
+      // postcss-pxtorem 插件的版本需要 >= 5.0.0
+      'postcss-pxtorem': {
+        rootValue({ file }) {
+          return file.indexOf('vant') !== -1 ? 37.5 : 75;
+        },
+        propList: ['*'],
+      },
+    },
   };
